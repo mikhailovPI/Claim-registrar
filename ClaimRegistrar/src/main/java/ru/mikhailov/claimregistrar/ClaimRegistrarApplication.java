511e23eaeeb8b97b0ac5d1c7d2a1cfaa4338af67
Mikhailov Pavel <mikhailov.pi@yandex.ru>
2023-04-01 11:11:23 +0300

    create project

---
+++ ClaimRegistrar/src/main/java/ru/mikhailov/claimregistrar/ClaimRegistrarApplication.java
@@ -0,0 +1,13 @@
+package ru.mikhailov.claimregistrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimRegistrarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimRegistrarApplication.class, args);
+	}
+
+}
